e58ee3611022834e358a1f722c6943fc1af11b32
sayan1108 <sayan.malda2.sc@gmail.com>
2024-02-22 01:40:48 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author='Sayan Chakraborty',
+    author_email='sayan.malda2.sc@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
